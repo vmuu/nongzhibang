408233ac81bcfb24a3726da191fe1a3ea2d232df
小星博博 <319653018@qq.com>
2021-05-08 22:09:35 +0800

    添加身份证照片上传界面

--- miniprogram/pages/mine/identification/identification.js
+++ miniprogram/pages/mine/identification/identification.js
@@ -1,11 +1,18 @@
 // miniprogram/pages/mine/identification/identification.js
+import utils from '../../../config/utils'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    entity: {
+      trueName: null,
+      id: null,
+      idPhotoFront: null,
+      idPhotoBack: null
 
+    }
   },
 
   /**
@@ -63,7 +70,39 @@ Page({
   onShareAppMessage: function () {
 
   },
-  tapCancel(){
+  tapCancel() {
     wx.navigateBack()
+  },
+  tapFront() {
+    let that = this
+    wx.chooseImage({
+      count: 1, //默认9
+      sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album'], //从相册选择
+      success: (res) => {
+        that.setData({
+          entity: {
+            idPhotoBack:that.data.entity.idPhotoBack,
+            idPhotoFront: res.tempFilePaths[0],
+          }
+        })
+      }
+    });
+  },
+  tapBack() {
+    let that = this
+    wx.chooseImage({
+      count: 1, //默认9
+      sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album'], //从相册选择
+      success: (res) => {
+        that.setData({
+          entity: {
+            idPhotoBack: res.tempFilePaths[0],
+            idPhotoFront:that.data.entity.idPhotoFront,
+          }
+        })
+      }
+    });
   }
 })
